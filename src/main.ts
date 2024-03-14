4e97917438bf9f7b150974427f94b84f4c671e27
PhamHuy <phamkhachuy240702@gmail.com>
2024-03-14 15:45:38 +0700

    create common

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import useToast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 const app = createApp(App)
 
 app.use(router)
-
+.use(useToast, {
+    position: 'top-right',
+    timeout: 1500,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+  })
 app.mount('#app')
